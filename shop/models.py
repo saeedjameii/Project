f1e234450b53bafe62ff09c75cd13b38cba57dcc
saeedjameii <saeed.jamei.amir@gmail.com>
2025-02-08 00:54:36 +0330

    migrate shop models

--- shop/models.py
+++ shop/models.py
@@ -7,7 +7,7 @@ class category(models.Model):
 class product(models.Model):
     title = models.CharField(max_length=100)
     contetnt = models.TextField()
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits= 10)
     image = models.ImageField()
     quantity = models.PositiveIntegerField()
     status = models.BooleanField(default=True)
@@ -21,21 +21,21 @@ class cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
 class order(models.Model):
-    total_price = models.DecimalField(decimal_places=2)
+    total_price = models.DecimalField(decimal_places=2, max_digits= 10)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.BooleanField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class order_products(models.Model):
     quantity = models.PositiveIntegerField()
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits= 10)
     # status = models.BooleanField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     order = models.ForeignKey(order, on_delete=models.CASCADE)
     product = models.ForeignKey(product, on_delete=models.CASCADE)
     
 class payment(models.Model):
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits= 10)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     order = models.ForeignKey(order, on_delete=models.CASCADE)
