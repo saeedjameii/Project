2c5bec6f2f11b8c8306f76c051321abf6e83bed2
saeedjameii <saeed.jamei.amir@gmail.com>
2025-07-10 19:50:37 +0330

    soft delete completed

--- shop/admin.py
+++ shop/admin.py
@@ -15,3 +15,38 @@ class CategoryAdmin(admin.ModelAdmin):
     def delete_queryset(self, request, queryset):
         for category in queryset:
             category.delete()
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_price', 'status')
+    list_filter = ('status',)
+
+    def delete_queryset(self, request, queryset):
+        for order in queryset:
+            order.delete()
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'quantity', 'status')
+    search_fields = ('title',)
+    list_filter = ('status',)
+
+    def delete_queryset(self, request, queryset):
+        for product in queryset:
+            product.delete()
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'user', 'quantity')
+    search_fields = ('product',)
+
+    def delete_queryset(self, request, queryset):
+        for cart in queryset:
+            cart.delete()
+
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order_id', 'product', 'quantity', 'price')
+
+    def delete_queryset(self, request, queryset):
+        for order_product in queryset:
+            order_product.delete()
+
--- shop/signals.py
+++ shop/signals.py
@@ -1,4 +1,4 @@
-from .models import Product , Cart
+from .models import Product , Cart, Order_products,Order
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -9,3 +9,20 @@ def soft_delete_cart(sender, instance,created, **kwargs):
             carts = Cart.objects.filter(product=instance)
             for cart in carts:
                 cart.delete()
+
+
+@receiver(post_save, sender=Product)
+def soft_delete_order_product(sender, instance,created, **kwargs):
+    if not created:
+        if instance.deleted:
+            order_products =  Order_products.objects.filter(product=instance)
+            for order_product in order_products:
+                order_product.delete()
+
+@receiver(post_save, sender=Order)
+def soft_delete_order_product(sender, instance,created, **kwargs):
+    if not created:
+        if instance.deleted:
+            order_products =  Order_products.objects.filter(order=instance)
+            for order_product in order_products:
+                order_product.delete()
