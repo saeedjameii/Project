1967665220435ca855dafd137238795d6240df6e
saeedjameii <saeed.jamei.amir@gmail.com>
2025-07-25 20:14:26 +0330

    link added to products list

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import index, store, checkout, detail
 
+app_name = 'shop'
+
 urlpatterns = [
     path('' , index, name="index"),
     path('store/', store, name="store"),
